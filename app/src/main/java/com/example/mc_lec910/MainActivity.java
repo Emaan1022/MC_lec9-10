29790a3e2d9ac0e07758fdcc508389dc0527fc6f
Emaan1022 <muqaddasrasheedkhan786@gmail.com>
2022-05-23 23:17:50 +0500

    adding files

--- app/src/main/java/com/example/mc_lec910/MainActivity.java
+++ app/src/main/java/com/example/mc_lec910/MainActivity.java
@@ -10,17 +10,58 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button button,button2,button3,button4,button5,button6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button=findViewById(R.id.button);
+        button=(Button) findViewById(R.id.button);
+        button2=(Button) findViewById(R.id.button2);
+        button3=(Button) findViewById(R.id.button3);
+        button4=(Button) findViewById(R.id.button4);
+        button5=(Button) findViewById(R.id.button5);
+        button6=(Button) findViewById(R.id.button6);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Uri uri =Uri.parse("tel:+923001234567");
-                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                Intent intent = new Intent(MainActivity.this, ImageActivity.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Image2Activity.class);
+                startActivity(intent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Image3Activity.class);
+                startActivity(intent);
+            }
+        });
+        //button4.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+          //  public void onClick(View view) {
+           //     Intent intent = new Intent(MainActivity.this, Image4Activity.class);
+           //     startActivity(intent);
+          //  }
+       // });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Image5Activity.class);
+                startActivity(intent);
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Image6Activity.class);
                 startActivity(intent);
             }
         });
